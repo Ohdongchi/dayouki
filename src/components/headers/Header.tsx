874264966271b8fe8f2007b3858ed00b1e3b5e66
Ohdongchi <dhqud1234@naver.com>
2023-08-31 12:01:48 +0900

    init

---
+++ src/components/headers/Header.tsx
@@ -0,0 +1,25 @@
+import Recat from "react";
+
+import PageIcon from "../../static/home.png";
+import UserIcon from "../../static/user.png"
+
+import "./style/header.scss";
+
+const Header = () => {
+    
+    return (
+        <header className="header">
+            <div className="header-box">
+                <div>
+                    <img src={PageIcon} alt="page-icon" className="header-icon" />
+                    <h2 className="header-logo">asdasd</h2>
+                </div>
+                <div>
+                    <img src={UserIcon} alt="header-user-info-icon" className="header-user-info-icon"/>
+                </div>            
+            </div>
+        </header>
+    )
+}
+
+export default Header;
